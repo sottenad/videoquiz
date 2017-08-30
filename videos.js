4132e5a867db4fcb7918eb85e4f9be3749a26b30
Autumn Ottenad <autumn@Edmodos-Air-2.hsd1.ca.comcast.net>
2017-08-30 15:25:46 -0700

    Adding video.js

---
+++ videos.js
@@ -0,0 +1,35 @@
+[
+    {
+        title: 'Introduction to Edmodo',
+        description: 'Today, we\'lllll guide you through the basics of using Edmodo to help administer your classroom and make life easier. Watch the introduction below before we get started exploring features.',
+        videoId: '-n7E2iyWRG8',
+        completed: false,
+        questions: [
+            {
+                text: 'What is this video about?',
+                answers: ['Edmodo', 'Teching Pedagogy', 'Setting up your iMac'],
+                time: 3,
+                correct: 0
+            }
+        ]
+    },
+    {
+        title: 'Your Edmodo Account',
+        videoId: 'plJJO8fLWfA',
+        completed: false,
+        questions: [
+            {
+                text: 'what am I?',
+                answers: ['a cat', 'a dog', 'a computer program'],
+                time: 3,
+                correct: 2
+            },
+            {
+                text: 'Follow up question?',
+                answers: ['1', '2', '3'],
+                time: 6,
+                correct: 0
+            }
+        ]
+    }
+]
